b3c2cb989bbc19e0b83a47600af9f306e5cdca0e
Robert von Massow <rvm@informatik.uni-kiel.de>
2010-08-26 18:37:48 +0200

    added logger output to LatexRunner workflow component

--- org.eclipse.xtext.xdoc.generator/src/workflow/LatexRunner.java
+++ org.eclipse.xtext.xdoc.generator/src/workflow/LatexRunner.java
@@ -1,17 +1,21 @@
 package workflow;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
 import org.eclipse.emf.mwe.core.WorkflowContext;
 import org.eclipse.emf.mwe.core.issues.Issues;
 import org.eclipse.emf.mwe.core.lib.AbstractWorkflowComponent;
-import org.eclipse.emf.mwe.core.lib.WorkflowComponentWithModelSlot;
 import org.eclipse.emf.mwe.core.monitor.ProgressMonitor;
 
 public class LatexRunner extends AbstractWorkflowComponent {
 
 	private String outputDir;
 	private String inputFile;
+	private Logger log = LogManager.getLogger(this.getClass());
 
 	@Override
 	protected void invokeInternal(WorkflowContext arg0, ProgressMonitor arg1,
@@ -19,9 +23,16 @@ public class LatexRunner extends AbstractWorkflowComponent {
 		try {
 			// arg1.beginTask("Invoking LaTeX builder on generated TeX file.",
 			// 0);
-			Runtime.getRuntime().exec(
+			Process p = Runtime.getRuntime().exec(
 					"pdflatex --output-directory " + outputDir + " "
 							+ inputFile);
+			BufferedReader is = new BufferedReader(new InputStreamReader(
+					p.getInputStream()));
+			String s = is.readLine();
+			while (s != null) {
+				log.info(s);
+				s = is.readLine();
+			}
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
