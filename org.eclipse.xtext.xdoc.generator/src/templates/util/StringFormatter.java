6a84180aa4973c500860ce3bb17c2ee95fb3e1fa
Robert von Massow <rvm@informatik.uni-kiel.de>
2010-09-15 19:42:11 +0200

    corrected indent of code blocks and top spacing of items, added Java extension for indentation cerrection

---
+++ org.eclipse.xtext.xdoc.generator/src/templates/util/StringFormatter.java
@@ -0,0 +1,31 @@
+package templates.util;
+
+import org.eclipse.xtext.xdoc.xdoc.Code;
+import org.eclipse.xtext.xdoc.xdoc.CodeBlock;
+
+public class StringFormatter {
+	
+	static public final CodeBlock removeIndent(CodeBlock cb){
+		if(cb.getContents().get(0) instanceof Code){
+			String code = ((Code)cb.getContents().get(0)).getContents();
+			int indent = code.length();
+			String[] split = code.split("\n");
+			
+			indent -= code.replaceAll("^(\n)?\\s*", "\\1").length();
+			if(indent <1)
+				return cb;
+			String string = "\n\\s{"+indent+"}";
+//			System.out.println(code);
+//			System.out.println(indent + string+"<--");
+			for(int i = 0; i < cb.getContents().size(); i++){
+				if (cb.getContents().get(i) instanceof Code) {
+					code = ((Code) cb.getContents().get(i)).getContents();
+					code = code.replaceAll(string, "\n");
+					((Code)cb.getContents().get(i)).setContents(code);
+//					System.out.println(code);
+				}
+			}
+		}
+		return cb;
+	}
+}
