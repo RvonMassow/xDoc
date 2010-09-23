4c10dd6a163f72e2d44c8b56027e8db8338c8b8b
Robert von Massow <rvm@informatik.uni-kiel.de>
2010-09-23 14:09:52 +0200

    added validator for linebreaks in code

--- org.eclipse.xtext.xdoc/src/org/eclipse/xtext/xdoc/validation/XDocJavaValidator.java
+++ org.eclipse.xtext.xdoc/src/org/eclipse/xtext/xdoc/validation/XDocJavaValidator.java
@@ -1,13 +1,20 @@
 package org.eclipse.xtext.xdoc.validation;
- 
+
+import org.eclipse.xtext.validation.Check;
+import org.eclipse.xtext.xdoc.xdoc.Code;
+import org.eclipse.xtext.xdoc.xdoc.XdocPackage;
 
 public class XDocJavaValidator extends AbstractXDocJavaValidator {
 
-//	@Check
-//	public void checkGreetingStartsWithCapital(Greeting greeting) {
-//		if (!Character.isUpperCase(greeting.getName().charAt(0))) {
-//			warning("Name should start with a capital", MyDslPackage.GREETING__NAME);
-//		}
-//	}
+	@Check
+	public void checkCodeLineLength(Code c) {
+		String[] lines = c.getContents().split("\n");
+		for (String string : lines) {
+			if (string.replaceAll("\t", "    ").length() >= 74) {
+				warning("Code line might break in PDF output",
+						XdocPackage.CODE__CONTENTS);
+			}
+		}
+	}
 
 }
