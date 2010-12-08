a90c4e1a11967f0ee3a14e538974b62a6ed8ebf8
Sebastian Zarnekow <Sebastian.Zarnekow@itemis.de>
2010-12-08 17:14:42 +0100

    [AutoEdit] insert linebreak when instead of a space if the line would exceed 100 visible characters

--- org.eclipse.xtext.xdoc.ui/src/org/eclipse/xtext/xdoc/ui/autoedit/AutoEditProvider.java
+++ org.eclipse.xtext.xdoc.ui/src/org/eclipse/xtext/xdoc/ui/autoedit/AutoEditProvider.java
@@ -1,12 +1,70 @@
 package org.eclipse.xtext.xdoc.ui.autoedit;
 
+import org.eclipse.jface.text.BadLocationException;
+import org.eclipse.jface.text.DocumentCommand;
+import org.eclipse.jface.text.IAutoEditStrategy;
 import org.eclipse.jface.text.IDocument;
+import org.eclipse.jface.text.IDocumentExtension4;
+import org.eclipse.jface.text.IRegion;
+import org.eclipse.swt.custom.StyledText;
+import org.eclipse.swt.events.VerifyEvent;
+import org.eclipse.swt.widgets.Widget;
+import org.eclipse.xtext.ui.editor.autoedit.AbstractEditStrategy;
 import org.eclipse.xtext.ui.editor.autoedit.DefaultAutoEditStrategyProvider;
 import org.eclipse.xtext.ui.editor.autoedit.MultiLineTerminalsEditStrategy;
 import org.eclipse.xtext.ui.editor.model.DocumentUtil;
 
 public class AutoEditProvider extends DefaultAutoEditStrategyProvider {
 	
+	protected static class LineBreakInserter extends AbstractEditStrategy {
+		
+		private Widget widget;
+		private final IAutoEditStrategy defaultStrategy;
+
+		protected LineBreakInserter(IAutoEditStrategy defaultStrategy) {
+			this.defaultStrategy = defaultStrategy;
+		}
+		
+		@Override
+		public void verifyKey(VerifyEvent event) {
+			super.verifyKey(event);
+			widget = event.widget;
+		}
+		
+		@Override
+		protected void internalCustomizeDocumentCommand(IDocument document,
+				DocumentCommand command) throws BadLocationException {
+			if (command.text.equals(" ") && command.length == 0) {
+				if (widget instanceof StyledText) {
+					StyledText text = (StyledText) widget;
+					IRegion region = document.getLineInformationOfOffset(command.offset);
+					String line = document.get(region.getOffset(), region.getLength());
+					String subLine = line.substring(0, command.offset - region.getOffset());
+					int tabCount = count("\t", subLine);
+					int spacesForTabs = text.getTabs() * tabCount;
+					int visibleOffset = subLine.length() - tabCount + spacesForTabs;
+					if (visibleOffset >= 100) {
+						command.text = ((IDocumentExtension4)document).getDefaultLineDelimiter();
+						defaultStrategy.customizeDocumentCommand(document, command);
+						if (document.getLineOfOffset(region.getOffset()) - text.getTopIndex() > 10)
+							text.setTopIndex(text.getTopIndex() + 1);
+					}
+				}
+			}
+			widget = null;
+		}
+	}
+
+	@Override
+	protected void configure(IEditStrategyAcceptor acceptor) {
+		super.configure(acceptor);
+		configureAutoLinebreakStrategy(acceptor);
+	}
+	
+	protected void configureAutoLinebreakStrategy(IEditStrategyAcceptor acceptor) {
+		acceptor.accept(new LineBreakInserter(defaultIndentLineAutoEditStrategy.get()), IDocument.DEFAULT_CONTENT_TYPE);
+	}
+
 	protected void configureMultilineComments(IEditStrategyAcceptor acceptor) {
 	}
 
