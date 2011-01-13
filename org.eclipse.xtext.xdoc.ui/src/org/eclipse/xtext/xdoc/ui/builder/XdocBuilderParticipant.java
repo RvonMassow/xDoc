9a9fe02ade1dcc1d58e93ef5b5a9b37522aafd86
Robert von Massow <rvm@informatik.uni-kiel.de>
2011-01-13 16:41:26 +0100

    fixed wrong extension loading

--- org.eclipse.xtext.xdoc.ui/src/org/eclipse/xtext/xdoc/ui/builder/XdocBuilderParticipant.java
+++ org.eclipse.xtext.xdoc.ui/src/org/eclipse/xtext/xdoc/ui/builder/XdocBuilderParticipant.java
@@ -1,5 +1,8 @@
 package org.eclipse.xtext.xdoc.ui.builder;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.apache.log4j.Logger;
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IFolder;
@@ -8,12 +11,15 @@ import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.Path;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.emf.mwe.core.resources.ResourceLoaderFactory;
+import org.eclipse.emf.mwe.core.resources.ResourceLoaderImpl;
 import org.eclipse.xpand2.XpandExecutionContextImpl;
 import org.eclipse.xpand2.XpandFacade;
 import org.eclipse.xpand2.output.FileHandle;
 import org.eclipse.xpand2.output.Outlet;
 import org.eclipse.xpand2.output.OutputImpl;
 import org.eclipse.xpand2.output.VetoException;
+import org.eclipse.xtend.expression.Variable;
 import org.eclipse.xtend.type.impl.java.JavaBeansMetaModel;
 import org.eclipse.xtext.builder.IXtextBuilderParticipant;
 import org.eclipse.xtext.resource.IResourceDescription;
@@ -37,39 +43,51 @@ public class XdocBuilderParticipant implements IXtextBuilderParticipant {
 			}
 		};
 		output.addOutlet(outlet);
-		XpandExecutionContextImpl ctx = new XpandExecutionContextImpl(output,
-				null, null, null, null);
-		ctx.registerMetaModel(new JavaBeansMetaModel());
+		Map<String, Variable> globalVars = new HashMap<String, Variable>();
+		try{
+			ResourceLoaderFactory.setCurrentThreadResourceLoader(new ResourceLoaderImpl(getClass().getClassLoader()));
+			XpandExecutionContextImpl ctx = new XpandExecutionContextImpl(output,
+					null, null, null, null);
+			ctx.registerMetaModel(new JavaBeansMetaModel());
 
-		String projectName = context.getBuiltProject().getName();
-		for (IResourceDescription.Delta delta : context.getDeltas()) {
-			// handle deletion
-			String projectSegment = delta.getUri().segment(1);
-			if (projectName.equals(projectSegment)) {
-				if (delta.getUri().fileExtension().equals("xdoc")) {
-					if (folder.get() == null) {
-						IFolder contentsFolder = context.getBuiltProject().getFolder("contents");
-						if (!contentsFolder.exists())
-							contentsFolder.create(true, true, monitor);
-						folder.set(contentsFolder);
-					}
-					if (delta.getNew() == null) {
-						IFile file = folder.get().getFile(delta.getUri().lastSegment()+".html");
-						if (file.exists())
-							file.delete(true, monitor);
-					} else {
-						Resource resource = context.getResourceSet().getResource(
-								delta.getUri(), true);
-						EObject object = resource.getContents().get(0);
-						generate(object, ctx, context);
+			String projectName = context.getBuiltProject().getName();
+			for (IResourceDescription.Delta delta : context.getDeltas()) {
+				// handle deletion
+				String projectSegment = delta.getUri().segment(1);
+				if (projectName.equals(projectSegment)) {
+					if (delta.getUri().fileExtension().equals("xdoc")) {
+						if (folder.get() == null) {
+							IFolder contentsFolder = context.getBuiltProject().getFolder("contents");
+							if (!contentsFolder.exists())
+								contentsFolder.create(true, true, monitor);
+							folder.set(contentsFolder);
+						}
+						if (delta.getNew() == null) {
+							IFile file = folder.get().getFile(delta.getUri().lastSegment()+".html");
+							if (file.exists())
+								file.delete(true, monitor);
+						} else {
+							Resource resource = context.getResourceSet().getResource(
+									delta.getUri(), true);
+							EObject object = resource.getContents().get(0);
+							Variable var = new Variable("srcDir", resource.getURI().path());
+							globalVars.put("srcDir", var);
+							var = new Variable("dir", "contents");
+							globalVars.put("dir", var);
+							ctx.getGlobalVariables().putAll(globalVars);
+							generate(object, ctx, context);
+						}
 					}
 				}
 			}
-		}
-		if(context.getBuildType().equals(BuildType.CLEAN)) {
-			IFile file = folder.get().getFile("toc.xml");
-			if (file.exists())
-				file.delete(true, monitor);
+			if(context.getBuildType().equals(BuildType.CLEAN)) {
+				IFile file = folder.get().getFile("toc.xml");
+				if (file.exists())
+					file.delete(true, monitor);
+			}
+		}finally{
+			ResourceLoaderFactory.setCurrentThreadResourceLoader(null);
+			ResourceLoaderFactory.setCurrentThreadResourceLoader(ResourceLoaderFactory.createResourceLoader());
 		}
 	}
 
