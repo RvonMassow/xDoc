4d0abe39b2fd64ad26ab24704802aa9e92fd02cc
Robert von Massow <rvm@informatik.uni-kiel.de>
2011-01-25 16:31:32 +0100

    started implementing tests for tex template, sugared tex template's output

---
+++ org.eclipse.xtext.xdoc.tests/src/org/eclipse/xtext/xdoc/tests/LatexGeneratorTest.java
@@ -0,0 +1,156 @@
+package org.eclipse.xtext.xdoc.tests;
+
+import java.util.Map;
+
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.xpand2.XpandFacade;
+import org.eclipse.xtend.expression.Variable;
+import org.eclipse.xtext.xdoc.xdoc.AbstractSection;
+import org.eclipse.xtext.xdoc.xdoc.Chapter;
+import org.eclipse.xtext.xdoc.xdoc.Document;
+import org.eclipse.xtext.xdoc.xdoc.TextOrMarkup;
+import org.eclipse.xtext.xdoc.xdoc.TextPart;
+import org.eclipse.xtext.xdoc.xdoc.XdocFactory;
+import org.eclipse.xtext.xdoc.xdoc.XdocFile;
+
+public class LatexGeneratorTest extends AbstractXdocGeneratorTest {
+
+
+	public void testSimpleDoc() throws Exception {
+		Document doc = initDoc("Simple Doc");
+		generate(doc);
+	}
+
+	@Override
+	public void testGenCode() throws Exception {
+		Document doc = initDocFromFile("code", "codeTest.xdoc");
+		generate(doc);
+	}
+
+	@Override
+	public void testGenCodeWithLanguage() throws Exception {
+		Document doc = initDocFromFile("codeWithLanguage", "codeWithLanguageTest.xdoc");
+		generate(doc);
+		validate(EXPECTATION_DIR + "codeWithLanguage.tex", RESULT_DIR + "Main.tex");
+	}
+
+	@Override
+	public void testARef() throws Exception {
+		Document doc = initDocFromFile("aRefTest", "aRefTest.xdoc");
+		generate(doc);
+	}
+
+	@Override
+	public void testCodeRef() throws Exception {
+		Document doc = initDocFromFile("codeRef", "codeRef.xdoc");
+		generate(doc);
+		validate(EXPECTATION_DIR + "codeRef.tex", RESULT_DIR + "codeRef.tex");
+	}
+
+	@Override
+	public void testComment() throws Exception {
+		Document doc = initDocFromFile("commentTest", "commentTest.xdoc");
+		generate(doc);
+		validate(EXPECTATION_DIR + "commentTest.tex", RESULT_DIR + "commentTest.tex");
+	}
+
+	@Override
+	public void testImg() throws Exception {
+		Document doc = initDocFromFile("imgTest", "imgTest.xdoc");
+		generate(doc);
+		validate(EXPECTATION_DIR + "imgTest.tex", RESULT_DIR + "imgTest.tex");
+	}
+
+	@Override
+	public void testLink() throws Exception {
+		Document doc = initDocFromFile("linkTest", "linkTest.xdoc");
+		generate(doc);
+		validate(EXPECTATION_DIR + "linkTest.tex", RESULT_DIR + "linkTest.tex");
+	}
+
+	@Override
+	public void testRefText() throws Exception {
+		Document doc = initDocFromFile("namedRefAndTextTest", "namedRefAndTextTest.xdoc");
+		generate(doc);
+		validate(EXPECTATION_DIR + "namedRefAndTextTest.tex", RESULT_DIR + "namedRefAndTextTest.tex");
+	}
+
+	@Override
+	public void testNestedList() throws Exception {
+		Document doc = initDocFromFile("nestedListTest", "nestedListTest.xdoc");
+		generate(doc);
+		validate(EXPECTATION_DIR + "nestedListTest.tex", RESULT_DIR + "nestedListTest.tex");
+	}
+
+	@Override
+	public void testSimpleRef() throws Exception {
+		Document doc = initDocFromFile("simpleRefTest", "simpleRefTest.xdoc");
+		generate(doc);
+		validate(EXPECTATION_DIR + "simpleRefTest.tex", RESULT_DIR + "simpleRefTest.tex");
+	}
+
+	@Override
+	public void testEscape() throws Exception {
+		Document doc = initDocFromFile("testEscape", "testEscape.xdoc");
+		generate(doc);
+		validate(EXPECTATION_DIR + "testEscape.tex", RESULT_DIR + "testEscape.tex");
+	}
+
+	@Override
+	public void testTable() throws Exception {
+		Document doc = initDocFromFile("table", "table.xdoc");
+		generate(doc);
+		validate(EXPECTATION_DIR + "table.tex", RESULT_DIR + "table.tex");
+	}
+
+	@Override
+	public void testTwoChapters() throws Exception {
+		Document doc = initDocFromFile("twoChapters", "twoChapters.xdoc");
+		generate(doc);
+		validate(EXPECTATION_DIR + "twoChapters.tex", RESULT_DIR + "twoChapters.tex");
+	}
+
+	@Override
+	public void testFullHirarchy() throws Exception {
+		Document doc = initDocFromFile("downToSection4Test", "downToSection4Test.xdoc");
+		generate(doc);
+		validate(EXPECTATION_DIR + "downToSection4Test.tex", RESULT_DIR + "downToSection4Test.tex");
+	}
+
+	private Document initDocFromFile(String string, String filename) throws Exception {
+		XdocFile file = pTest.getDocFromFile(SRC_DIR + filename);
+		AbstractSection mainSection = file.getMainSection();
+		if(mainSection instanceof Document) {
+			return (Document) mainSection;
+		} else if(mainSection instanceof Chapter) {
+			Document doc = initDoc(string);
+			doc.getChapters().add((Chapter) mainSection);
+			return doc;
+		}
+		return null;
+	}
+
+	private Document initDoc(String name) {
+		Document result = XdocFactory.eINSTANCE.createDocument();
+		TextOrMarkup tomTitle = XdocFactory.eINSTANCE.createTextOrMarkup();
+		TextPart title = XdocFactory.eINSTANCE.createTextPart();
+		title.setText(name);
+		tomTitle.getContents().add(title);
+		result.setTitle(tomTitle);
+		return result;
+	}
+
+	@Override
+	protected void generate(EObject eObject) {
+		generate(eObject, "false");
+	}
+
+	private void generate(EObject obj, String release) {
+		Map<String, Variable> vars = getXpandCtx().getGlobalVariables();
+		Variable var = new Variable("release", release);
+		vars.put("release", var);
+		XpandFacade.create(getXpandCtx()).evaluate(
+				"templates::Template::main", obj);
+	}
+
+}
