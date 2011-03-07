a5e8c151ab3ba754eb1edf2ec0425179bd1c8f0f
Robert von Massow <rvm@informatik.uni-kiel.de>
2011-03-07 13:26:08 +0100

    forgot resource description strategy

---
+++ org.eclipse.xtext.xdoc/src/org/eclipse/xtext/xdoc/resource/XdocResourceDescriptionStrategy.java
@@ -0,0 +1,38 @@
+package org.eclipse.xtext.xdoc.resource;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.log4j.Logger;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.xtext.naming.QualifiedName;
+import org.eclipse.xtext.resource.EObjectDescription;
+import org.eclipse.xtext.resource.IEObjectDescription;
+import org.eclipse.xtext.resource.impl.DefaultResourceDescriptionStrategy;
+import org.eclipse.xtext.util.IAcceptor;
+import org.eclipse.xtext.xdoc.XdocUtil;
+
+public class XdocResourceDescriptionStrategy extends
+		DefaultResourceDescriptionStrategy {
+
+	private final static Logger LOG = Logger.getLogger(XdocResourceDescriptionStrategy.class);
+
+	@Override
+	public boolean createEObjectDescriptions(EObject eObject,
+			IAcceptor<IEObjectDescription> acceptor) {
+		if (getQualifiedNameProvider() == null)
+			return false;
+		try {
+			QualifiedName qualifiedName = getQualifiedNameProvider().getFullyQualifiedName(eObject);
+			if (qualifiedName != null) {
+				Map<String, String> userData = new HashMap<String, String>();
+				System.out.println(eObject.eContents().toString().hashCode());
+				userData.put("hash", "" + XdocUtil.toString(eObject).hashCode());
+				acceptor.accept(EObjectDescription.create(qualifiedName, eObject, userData));
+			}
+		}catch (Exception e) {
+			LOG.error(e);
+		}
+		return true;
+	}
+}
