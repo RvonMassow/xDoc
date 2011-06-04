0e3353d086bb5b8fb19d0931e443447b003f25e7
Sebastian Zarnekow <Sebastian.Zarnekow@itemis.de>
2011-06-04 10:40:10 +0200

    Some validation rules, updated build properties and .classpath

--- org.eclipse.xtext.xdoc/src/org/eclipse/xtext/xdoc/validation/XdocJavaValidator.java
+++ org.eclipse.xtext.xdoc/src/org/eclipse/xtext/xdoc/validation/XdocJavaValidator.java
@@ -4,10 +4,16 @@ import java.util.Collections;
 import java.util.List;
 
 import org.eclipse.emf.common.util.EList;
+import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.EPackage;
+import org.eclipse.emf.ecore.resource.ResourceSet;
+import org.eclipse.xtext.nodemodel.ICompositeNode;
+import org.eclipse.xtext.nodemodel.util.NodeModelUtils;
 import org.eclipse.xtext.validation.Check;
 import org.eclipse.xtext.validation.ValidationMessageAcceptor;
 import org.eclipse.xtext.xdoc.xdoc.Code;
+import org.eclipse.xtext.xdoc.xdoc.CodeRef;
+import org.eclipse.xtext.xdoc.xdoc.ImageRef;
 import org.eclipse.xtext.xdoc.xdoc.Table;
 import org.eclipse.xtext.xdoc.xdoc.TableRow;
 import org.eclipse.xtext.xdoc.xdoc.XdocPackage;
@@ -43,6 +49,43 @@ public class XdocJavaValidator extends AbstractXdocJavaValidator {
 		}
 	}
 
+	@Check
+	public void checkSuspiciousCodeRef(CodeRef codeRef) {
+		ICompositeNode node = NodeModelUtils.findActualNodeFor(codeRef);
+		String completeText = node.getRootNode().getText();
+		if (node.getTotalEndOffset() < completeText.length() - 1) {
+			int endOffset = node.getOffset() + node.getLength();
+			char c = completeText.charAt(endOffset);
+			if (!Character.isWhitespace(c) && c != ',' && c != '.' && c != ']' && c != ':' && c != '-') {
+				error("Code Reference is followed by a suspicious character.", null);
+			}
+			if (c == '.' && endOffset + 1 < completeText.length() && Character.isJavaIdentifierStart(completeText.charAt(endOffset + 1))) {
+				error("Code Reference is followed by a suspicious character sequence.", null);
+			}
+		}
+	}
+	
+//	@Check
+//	public void checkSuspiciousEmphasize(Emphasize e) {
+//		ICompositeNode node = NodeModelUtils.findActualNodeFor(e);
+//		String completeText = node.getRootNode().getText();
+//		String nodeText = completeText.substring(node.getOffset(), node.getOffset() + node.getLength());
+//		nodeText = nodeText.substring(2, nodeText.length() - 1);
+//		if (nodeText.length() > 0) {
+//			if (nodeText.endsWith(")"))
+//				warning("Suspicious content in emphasize", null); 
+//		}
+//	}
+	
+	@Check
+	public void checkImagePath(ImageRef image) {
+		ResourceSet resourceSet = image.eResource().getResourceSet();
+		URI resolvedURI = URI.createURI(image.getPath()).resolve(image.eResource().getURI());
+		if (!resourceSet.getURIConverter().exists(resolvedURI, null)) {
+			error("Cannot find image", XdocPackage.Literals.IMAGE_REF__PATH);
+		}
+	}
+	
 	@Override
 	protected List<EPackage> getEPackages() {
 		return Collections.singletonList((EPackage)XdocPackage.eINSTANCE);
