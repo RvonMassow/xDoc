d60ef2ce79644b8ef38eae597171b9e915d3d3ce
Robert von Massow <rvm@informatik.uni-kiel.de>
2011-08-17 11:20:11 +0200

    fixed some LaTeX generator issues (scaling of images, linebreaks in coderefs)

---
+++ org.eclipse.xtext.xdoc.generator/src/templates/util/PDFImageUtil.java
@@ -0,0 +1,45 @@
+package templates.util;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Iterator;
+
+import javax.imageio.ImageIO;
+import javax.imageio.ImageReader;
+import javax.imageio.metadata.IIOMetadata;
+import javax.imageio.stream.ImageInputStream;
+
+import org.eclipse.emf.common.util.URI;
+import org.eclipse.xtext.xdoc.xdoc.ImageRef;
+import org.w3c.dom.Node;
+
+public class PDFImageUtil {
+
+	public static String calcStyle(ImageRef ref) throws IOException {
+		URI uri = ref.eResource().getURI();
+		URI inPath = URI.createURI(uri.trimSegments(1).toString() + "/" + ref.getPath());
+		ImageInputStream img = ImageIO.createImageInputStream(new File(inPath.toFileString()));
+		Iterator<ImageReader> imageReaders = ImageIO.getImageReaders(img);
+		ImageReader ir = imageReaders.next();
+		ir.setInput(img);
+		IIOMetadata imageMetadata = ir.getImageMetadata(0);
+		Node n = imageMetadata.getAsTree("javax_imageio_1.0");
+		float ppmm = 2.835f;
+		for(Node cn = n.getFirstChild(); cn != null; cn = cn.getNextSibling()) {
+			if(cn.getNodeName().equals("Dimension")) {
+				for(Node ccn = cn.getFirstChild(); ccn != null; ccn = ccn.getNextSibling()){
+					if(ccn.getNodeName().equals("HorizontalPixelSize")){
+						// 6.3 is a good value here, but lets make it 6.0 - 6.6
+						ppmm = 1/ Float.parseFloat(ccn.getAttributes().item(0).getNodeValue());
+					}
+				}
+			}
+		}
+		int width = ir.getWidth(0);
+		float effectiveWidth = width/ppmm;
+		if(effectiveWidth > 140){
+			return "width=\\textwidth";
+		} else
+			return "";
+	}
+}
