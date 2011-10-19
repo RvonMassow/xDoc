f946ef8df6a7d265c4cf546a64d14f73869662e4
Robert von Massow <rvm@informatik.uni-kiel.de>
2011-10-19 14:03:40 +0200

    added missing Config interface

---
+++ org.eclipse.xtext.xdoc/src/org/eclipse/xtext/xdoc/generator/config/Config.java
@@ -0,0 +1,8 @@
+package org.eclipse.xtext.xdoc.generator.config;
+
+public interface Config {
+	public static final String outletPath = "oPath";
+	public static final String release = "release";
+	public static final String cleanAfterRun = "cleanAfterRun";
+	public static final String BASE_DIR = "baseDir";
+}
