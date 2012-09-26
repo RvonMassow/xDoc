ba3bea558c6c5ec9f7ebc38695a9683e73a1d256
Christoph Kulla <christophkulla@googlemail.com>
2012-09-26 19:58:18 +0200

    Added feature projects to pom Added update site project to pom Made tests compile (tests still fail) Cleanup of gitignore files

--- org.eclipse.xtext.xdoc.tests/src/org/eclipse/xtext/xdoc/tests/UtilityTest.java
+++ org.eclipse.xtext.xdoc.tests/src/org/eclipse/xtext/xdoc/tests/UtilityTest.java
@@ -9,25 +9,25 @@ import org.eclipse.xtext.xdoc.xdoc.CodeBlock;
 import org.eclipse.xtext.xdoc.xdoc.LangDef;
 import org.eclipse.xtext.xdoc.xdoc.XdocFactory;
 
-import templates.util.StringFormatter;
+//import templates.util.StringFormatter;
 import junit.framework.TestCase;
 
 public class UtilityTest extends TestCase {
 
-	String testStringSplitting =
-	"&nbsp;grammar&nbsp;org.eclipse.xtext.common.Terminals&nbsp;<br />\n" +
-	"&nbsp;hidden(WS,&nbsp;ML_COMMENT,&nbsp;SL_COMMENT)<br />\n" +
-	"import&nbsp;\"http://www.eclipse.org/emf/2002/Ecore\"&nbsp;as&nbsp;ecore<br />\n" +
-	"terminal&nbsp;ID&nbsp;:&nbsp;<br />\n" +
-	"&nbsp;&apos;^&apos;?(&apos;a&apos;..&apos;z&apos;|&apos;A&apos;..&apos;Z&apos;|&apos;_&apos;)&nbsp;(&apos;a&apos;..&apos;z&apos;|&apos;A&apos;..&apos;Z&apos;|&apos;_&apos;|&apos;0&apos;..&apos;9&apos;)*&nbsp;;<br />\n" +
-	"terminal&nbsp;INT&nbsp;returns&nbsp;ecore::EInt:&nbsp;(&apos;0&apos;..&apos;9&apos;)+&nbsp;;<br />\n" +
-	"terminal&nbsp;STRING&nbsp;&nbsp;&nbsp;&nbsp;:&nbsp;<br />\n" +
-	"&nbsp;&apos;\"&apos;&nbsp;(&nbsp;&apos;\\\\&apos;&nbsp;(&apos;b&apos;|&apos;t&apos;|&apos;n&apos;|&apos;f&apos;|&apos;r&apos;|&apos;\"&apos;|\"&apos;\"|&apos;\\\\&apos;)&nbsp;|&nbsp;!(&apos;\\&apos;|&apos;\"&apos;)&nbsp;)*&nbsp;&apos;\"&apos;&nbsp;|<br />\n" +
-	"&nbsp;\"&apos;\"&nbsp;(&nbsp;&apos;\\\\&apos;&nbsp;(&apos;b&apos;|&apos;t&apos;|&apos;n&apos;|&apos;f&apos;|&apos;r&apos;|&apos;\"&apos;|\"&apos;\"|&apos;\\\\&apos;)&nbsp;|&nbsp;!(&apos;\\\\&apos;|\"&apos;\")&nbsp;)*&nbsp;\"&apos;\";&nbsp;<br />\n" +
-	"terminal&nbsp;ML_COMMENT&nbsp;&nbsp;&nbsp;&nbsp;:&nbsp;&apos;/*&apos;&nbsp;-&gt;&nbsp;&apos;*/&apos;&nbsp;;<br />\n" +
-	"terminal&nbsp;SL_COMMENT&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;:&nbsp;&apos;//&apos;&nbsp;!(&apos;\\n&apos;|&apos;\\r&apos;)*&nbsp;(&apos;\\r&apos;?&nbsp;&apos;\\n&apos;)?&nbsp;;<br />\n" +
-	"terminal&nbsp;WS&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;:&nbsp;(&apos;&nbsp;&apos;|&apos;\\t&apos;|&apos\\;\\r&apos;|&apos;\\n&apos;)+&nbsp;;<br />\n" +
-	"terminal&nbsp;ANY_OTHER:&nbsp;&nbsp;&nbsp;&nbsp;.&nbsp;;";
+//	String testStringSplitting =
+//	"&nbsp;grammar&nbsp;org.eclipse.xtext.common.Terminals&nbsp;<br />\n" +
+//	"&nbsp;hidden(WS,&nbsp;ML_COMMENT,&nbsp;SL_COMMENT)<br />\n" +
+//	"import&nbsp;\"http://www.eclipse.org/emf/2002/Ecore\"&nbsp;as&nbsp;ecore<br />\n" +
+//	"terminal&nbsp;ID&nbsp;:&nbsp;<br />\n" +
+//	"&nbsp;&apos;^&apos;?(&apos;a&apos;..&apos;z&apos;|&apos;A&apos;..&apos;Z&apos;|&apos;_&apos;)&nbsp;(&apos;a&apos;..&apos;z&apos;|&apos;A&apos;..&apos;Z&apos;|&apos;_&apos;|&apos;0&apos;..&apos;9&apos;)*&nbsp;;<br />\n" +
+//	"terminal&nbsp;INT&nbsp;returns&nbsp;ecore::EInt:&nbsp;(&apos;0&apos;..&apos;9&apos;)+&nbsp;;<br />\n" +
+//	"terminal&nbsp;STRING&nbsp;&nbsp;&nbsp;&nbsp;:&nbsp;<br />\n" +
+//	"&nbsp;&apos;\"&apos;&nbsp;(&nbsp;&apos;\\\\&apos;&nbsp;(&apos;b&apos;|&apos;t&apos;|&apos;n&apos;|&apos;f&apos;|&apos;r&apos;|&apos;\"&apos;|\"&apos;\"|&apos;\\\\&apos;)&nbsp;|&nbsp;!(&apos;\\&apos;|&apos;\"&apos;)&nbsp;)*&nbsp;&apos;\"&apos;&nbsp;|<br />\n" +
+//	"&nbsp;\"&apos;\"&nbsp;(&nbsp;&apos;\\\\&apos;&nbsp;(&apos;b&apos;|&apos;t&apos;|&apos;n&apos;|&apos;f&apos;|&apos;r&apos;|&apos;\"&apos;|\"&apos;\"|&apos;\\\\&apos;)&nbsp;|&nbsp;!(&apos;\\\\&apos;|\"&apos;\")&nbsp;)*&nbsp;\"&apos;\";&nbsp;<br />\n" +
+//	"terminal&nbsp;ML_COMMENT&nbsp;&nbsp;&nbsp;&nbsp;:&nbsp;&apos;/*&apos;&nbsp;-&gt;&nbsp;&apos;*/&apos;&nbsp;;<br />\n" +
+//	"terminal&nbsp;SL_COMMENT&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;:&nbsp;&apos;//&apos;&nbsp;!(&apos;\\n&apos;|&apos;\\r&apos;)*&nbsp;(&apos;\\r&apos;?&nbsp;&apos;\\n&apos;)?&nbsp;;<br />\n" +
+//	"terminal&nbsp;WS&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;:&nbsp;(&apos;&nbsp;&apos;|&apos;\\t&apos;|&apos\\;\\r&apos;|&apos;\\n&apos;)+&nbsp;;<br />\n" +
+//	"terminal&nbsp;ANY_OTHER:&nbsp;&nbsp;&nbsp;&nbsp;.&nbsp;;";
 
 	private LangDef testLangDef;
 
@@ -37,7 +37,7 @@ public class UtilityTest extends TestCase {
 									"\t\tstatic int foo = 13;\n" +
 									"\t}\t\n\n  ";
 
-	private String expectationCodeString = "class Foo{\n\tstatic int foo = 13;\n}";
+	// private String expectationCodeString = "class Foo{\n\tstatic int foo = 13;\n}";
 
 	private CodeBlock testCodeBlock;
 
@@ -54,25 +54,25 @@ public class UtilityTest extends TestCase {
 		testCodeBlock.getContents().add(testCode);
 	}
 
-	public void testSplitting() throws Exception {
-		String text = testStringSplitting;
-		String[] toks;
-		do {
-			toks = StringFormatter.splitToNext(text);
-			if(toks.length == 3){
-				assertFalse("infinite loop: " + toks[2] + " == " + text, toks[2].equals(text));
-				text = toks[2];
-			}
-		} while(toks.length == 3);
-	}
-
-	public void testRemoveIndent() throws Exception {
-		CodeBlock result = StringFormatter.removeIndent(this.testCodeBlock);
-		assertEquals(1, result.getContents().size());
-		assertTrue(result.getContents().get(0) instanceof Code);
-		Code resultCode = (Code) result.getContents().get(0);
-		assertEquals(expectationCodeString, resultCode.getContents());
-	}
+//	public void testSplitting() throws Exception {
+//		String text = testStringSplitting;
+//		String[] toks;
+//		do {
+//			toks = StringFormatter.splitToNext(text);
+//			if(toks.length == 3){
+//				assertFalse("infinite loop: " + toks[2] + " == " + text, toks[2].equals(text));
+//				text = toks[2];
+//			}
+//		} while(toks.length == 3);
+//	}
+//
+//	public void testRemoveIndent() throws Exception {
+//		CodeBlock result = StringFormatter.removeIndent(this.testCodeBlock);
+//		assertEquals(1, result.getContents().size());
+//		assertTrue(result.getContents().get(0) instanceof Code);
+//		Code resultCode = (Code) result.getContents().get(0);
+//		assertEquals(expectationCodeString, resultCode.getContents());
+//	}
 	
 	public void testFormatCode() throws Exception {
 		LangDef langDef = XdocFactory.eINSTANCE.createLangDef();
